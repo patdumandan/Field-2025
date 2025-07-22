d6a61c0dec1f7d6349ab7201773b2027fd0247d8
Patricia <patdumandan@gmail.com>
2025-07-22 20:04:04 +0000

    make changing filepath easier

--- data_exploration.R
+++ data_exploration.R
@@ -3,38 +3,25 @@ require(tidyr)
 require(ggplot2)
 
 #wolf spiders
-loc_dat=read.csv("C:\\pdumandanSLU\\PatD-SLU\\SLU\\fieldwork\\2025\\Field-2025\\raw_data\\locomotion_2025.csv",
-                header=T, sep=",")
+filepath = "C:\\pdumandanSLU\\PatD-SLU\\SLU\\fieldwork\\2025\\Field-2025\\raw_data"
 
+filename = paste(filepath, '\\locomotion_2025','.csv', sep = '')
+
+loc_dat=read.csv(filename,header=T, sep=",")
+
+#locomotion####
 loc_dat=loc_dat%>%
   mutate(movement=if_else(Distance_cm<1, "no", "yes"),
-         speed=Distance_cm/Time_sec)
+         speed=Distance_cm/Time_sec,
+         Size_cat=case_when(Weight_g<0.04 ~"S",
+                            Weight_g>0.04 ~"L"))
 
-ws_dat=loc_dat%>%filter(Taxon=="wolf_spider")
+##wolf spider####
+
+ws_dat=loc_dat%>%filter(Taxon=="wolf_spider", loc_temp<45)
 
 ws_dat$Time_sec[is.na(ws_dat$Time_sec)] <- 0
 ws_dat$Distance_cm[is.na(ws_dat$Distance_cm)] <- 0
-
-wv_dat=loc_dat%>%filter(Taxon=="weevil")
-mu_dat=loc_dat%>%filter(Taxon=="muscids")
-
-ws_dat_b=ws_dat%>%filter(Size_cat=="L")
-ws_dat_size=ws_dat%>%filter(Size_cat%in%c("L","S"))
-
-#plot
-ggplot(ws_dat_b, aes(x=loc_temp, y=speed))+
-  geom_point()+
-  geom_smooth(method="gam")+
-  theme_classic()+
-  ylab("speed (cm/s)")+xlab("Temperature(C)")+
-  ggtitle("wolf spider (large)")
-
-ggplot(ws_dat_size, aes(x=loc_temp, y=speed, col=Size_cat))+
-  geom_point()+
-  geom_smooth(method="gam")+
-  theme_classic()+
-  ylab("speed (cm/s)")+xlab("Temperature(C)")+
-  ggtitle("wolf spider")
 
 ggplot(ws_dat, aes(x=loc_temp, y=speed, col=Location))+
   geom_point(aes(col=Location))+ 
@@ -43,6 +30,9 @@ ggplot(ws_dat, aes(x=loc_temp, y=speed, col=Location))+
   ylab("speed (cm/s)")+xlab("Temperature(C)")+
   ggtitle("wolf spider (all)")
 
+##weevil####
+wv_dat=loc_dat%>%filter(Taxon=="weevil")
+
 ggplot(wv_dat, aes(x=loc_temp, y=speed))+
   geom_point()+
   geom_smooth(method="gam")+
@@ -50,10 +40,13 @@ ggplot(wv_dat, aes(x=loc_temp, y=speed))+
   ylab("speed (cm/s)")+xlab("Temperature(C)")+
   ggtitle("weevil")
 
+##muscid####
+mu_dat=loc_dat%>%filter(Taxon=="muscids")
+
 ggplot(mu_dat, aes(x=loc_temp, y=speed, col=Location))+
   geom_point(aes(col=Location))+
   geom_smooth(method="gam")+
-  theme_classic()+facet_wrap(~Location)+
+  theme_classic()+
   ylab("speed (cm/s)")+xlab("Temperature(C)")+
   ggtitle("muscids")
 
@@ -69,6 +62,7 @@ hkdt_dat=loc_dat%>%filter(test=="HKDT", Temp%in%c(35:45))%>%
 hkdt_dat$end_time_s=as.numeric(hkdt_dat$end_time_s)
 
 
-ggplot(hkdt_dat, aes(x=Temp, y=end_time_s))+
-  geom_boxplot(aes(col=acclim_type))+facet_wrap(~Taxon)+
-  theme_classic()+ylab("HKDT")+xlab("Temperature")
+ggplot(hkdt_dat, aes(x=Temp, y=end_time_s, ))+
+  geom_boxplot(aes(fill=acclim_type))+facet_wrap(~Taxon)+
+  theme_classic()+ylab("HKDT")+xlab("Temperature")+
+  scale_fill_manual(values=c("cold"="blue", "hot"="red"))
--- mozzie-data_exploration.R
+++ mozzie-data_exploration.R
@@ -1,23 +1,57 @@
-moz1=read.csv("C:\\pdumandanSLU\\PatD-SLU\\SLU\\fieldwork\\2025\\Field-2025\\raw_data\\Aedes\\Aedes-devt-rate_2025.csv", 
-              header=T, sep=",")
-
 require(dplyr)
 require(tidyr)
+library(stringr)
+require(lubridate)
 
+#load data###
+filepath = "C:\\pdumandanSLU\\PatD-SLU\\SLU\\fieldwork\\2025\\Field-2025\\raw_data\\Aedes"
 
-moz_dat=moz1%>%
-  pivot_longer(cols=3:13, names_to = "date")%>%
-  rename(status=value)%>%
-  mutate(month=substr(date, 2, 3),
-         day=substr(date, 5,6),
-         year=substr(date, 8, 9),
-         Year=case_match(year, "25"~"2025"),
-         Date=lubridate::make_date(Year, month, day))%>%
-  select(-date)
+filename = paste(filepath, '\\Aedes-devt-rate_2025','.csv', sep = ',')
 
-moz_dat$month=as.integer(moz_dat$month)
-moz_dat$day=as.integer(moz_dat$day)
-moz_dat$year=as.integer(moz_dat$year)
+tempdat=paste(filepath, '\\T1-10C','.txt', sep = '')
+
+moz1=read.csv(filename, header=T, sep=",", stringsAsFactors = F)
+tempdat1=read.csv(tempdat, fileEncoding = "Latin1", header=T)
+
+#reconfigure table for analysis###
+
+moz_dat <- moz1 %>%
+  pivot_longer(cols = 3:23, names_to = "date") %>% #make sure to increase cols end until end of experiment
+  rename(status = value) %>%
+  mutate(
+    clean_date = stringr::str_remove_all(date, "X") %>% str_replace_all("\\.", "-"),
+    Date = mdy(clean_date)) %>%
+  select(-date, -clean_date)
+
+moz_dat$Date=as.Date(moz_dat$Date)
+
+library(dplyr)
+library(tidyr)
+
+moz_prop <- moz_dat %>%
+  group_by(Temperature, Date, status) %>%
+  summarise(Count = n(), .groups = "drop") %>%
+  pivot_wider(names_from = status, values_from = Count, values_fill = 0) %>%  # Fill NAs with 0
+  rowwise() %>%
+  mutate(Total = sum(c_across(3:6)),
+         larva=L/Total,
+         pupa=P/Total,
+         adult=A/Total,
+         dead=D/Total)%>%
+  select(-L, -P, -D, -A)%>%
+  pivot_longer(cols=4:7, names_to="stage")
+
+#plot data###
+
+require(ggplot2)
+
+ggplot(moz_prop, aes(x=Date, y=value, fill=stage))+
+  geom_col()+#geom_line()+
+  theme_classic()+
+  facet_wrap(~Temperature)+
+  ylab("proportion")+
+  scale_color_viridis_c()
+
 moz_dat$Date=as.Date(moz_dat$Date)
 
 
@@ -27,10 +61,12 @@ moz_all= moz_dat %>%
   ungroup() %>%
   group_by(Temperature, Date) %>%
   mutate(Total = n()) %>%
+  arrange(Temperature, Date)%>%
   group_by(Temperature, Date, status) %>%
   mutate(CumSum = cumsum(1),
          CumFrac = CumSum / Total) %>%
-  drop_na()
+  drop_na()%>%
+  select(Date, Temperature, CumFrac)
 
 require(ggplot2)
 
